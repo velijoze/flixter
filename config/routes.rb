a69ddd87d4000a8dc271969e4c013e4361c023f2
velijoze <velijoze@outlook.com>
2017-07-16 18:06:52 +0000

    Listing courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
-  	resources :courses, only: [:new, :create, :show]
+    resources :courses, only: [:new, :create, :show]
   end
 end
